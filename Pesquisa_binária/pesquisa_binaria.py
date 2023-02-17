f82b6f5e05cacada13984e9b342f3e28a12230fe
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-02-17 11:19:34 -0300

    Adicona comentários no código de pesquisa binária

--- Pesquisa_binária/pesquisa_binaria.py
+++ Pesquisa_binária/pesquisa_binaria.py
@@ -18,19 +18,26 @@ para retornar o valor correto, enquanto a pesquisa simples precisa de n etapas.
 """
 
 def pesquisa_binaria(lista, item):
+    # baixo e alto acompanham a parte da lista que você está procurando.
     baixo = 0
     alto = len(lista) - 1
     
+    # Enquanto ainda não conseguiu chegar a um único elemento...
     while baixo <= alto:
+        # ... verifica o elemento central
         meio = int((baixo + alto) / 2)
         chute = lista[meio]
         
+        # Acha o item
         if chute == item:
             return meio
+        # O chute foi muito alto
         if chute > item:
             alto = meio - 1
+        # O chute foi muito baixo
         else:
             baixo = meio + 1
+    # O item não existe
     return None
 
 minha_lista = [1, 3, 5, 7, 9]
